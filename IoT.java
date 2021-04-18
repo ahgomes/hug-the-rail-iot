0eedc7465c233db1f8393b628d94d39f812ed952
Adrian Gomes <agomes2@stevens.edu>
2021-04-18 04:22:26 -0400

    Set up files for IoT program

---
+++ IoT.java
@@ -0,0 +1,49 @@
+/**
+ *  IoT.java runs a simulation of the Hug the Rails IoT system as defined in
+ *  the IoT HRT Project Document.
+ *
+ *  I pledge my honor that I have abided by the Stevens Honor System.
+ *
+ *  @author Adrian Gomes, Aliya Iqbal, Amraiza Naz, and Matthew Cunningham
+ *  @versio 1.0
+ *  @since 2021-04-19
+ */
+
+public class IoT {
+
+    class Display {
+
+        public Display() {
+
+        }
+    }
+
+    class TSNR {
+
+        public TSNR() {
+
+        }
+    }
+
+    class Sensor {
+
+        public Sensor() {
+
+        }
+    }
+
+    class Log {
+
+        public Log() {
+
+        }
+    }
+
+    public IoT() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ IoTTest.java
@@ -0,0 +1,17 @@
+/**
+ *  IoTTest.java tests IoT.java against unit tests that correspond to the
+ *  use cases defined in the IoT HRT Project Document.
+ *
+ *  I pledge my honor that I have abided by the Stevens Honor System.
+ *
+ *  @author Adrian Gomes, Aliya Iqbal, Amraiza Naz, and Matthew Cunningham
+ *  @versio 1.0
+ *  @since 2021-04-19
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IoTTest.java {
+
+}
